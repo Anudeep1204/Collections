25febcb194763c3ee21692e847d5c18c2c27198f
sys-51 <sys-51@sys-50-PC>
2018-03-23 13:37:42 +0530

    Collections

--- src/ArrayListMethods/ArrayListClient.java
+++ src/ArrayListMethods/ArrayListClient.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ListIterator;
 
-public class ArrayListClient {
+public class ArrayListClient extends ArrayList {
 
 	public static void main(String[] args) {
 		ArrayList al=new ArrayList();
 		ArrayList al1=new ArrayList();
 		ArrayList al2=new ArrayList();
+		ArrayList al3=new ArrayList();
 		
 		System.out.println("***********ArrayList Methods**************");
 		al.add(20);
@@ -67,8 +68,12 @@ public class ArrayListClient {
 		
 		System.out.println("LastIndexOF : "+al.lastIndexOf(28));
 		//last occurance of 28 in al is 7th index postion first occurance is 4th position
-		
+
+		ListIterator litr1=al.listIterator();
 		ListIterator litr=al.listIterator(5);
+		while(litr1.hasNext()) {
+			System.out.println("List Iterator total : "+litr1.next());
+		}
 		while(litr.hasNext()) {
 			System.out.println("List Iterator Forward : "+litr.next());
 		}
@@ -76,23 +81,63 @@ public class ArrayListClient {
 			System.out.println("List Iterator Backward : "+litr.previous());
 		}
 		
+		
 		al.remove(2);
 		System.out.println("After removing 2 index 25 in al : "+al);
 		
-		//al2.removeRange(2,5);
-		//protected void
+		//for integer elements you have to mention new Integer as object
+		al.remove(new Integer(28));
+		System.out.println("After removing 28 al : "+al);
+		//for characters no need of object
+		al.add("E");
+		al.add(54);
+		al.add(120);
+		al.add("E");
+		System.out.println("After adding E in al: "+al);
+		al.remove("E");
+		System.out.println("After removing E in al : "+al);
 		
-	/*	System.out.println("After  Retain All elements in al1 : "+al1.retainAll(al));
-		System.out.println(al1);
+		al3.addAll(al);
+		System.out.println("After Adding al Elements to al3"+al3);
+		al3.removeAll(al);
+		System.out.println("After Removing Elements of al in al3"+al3);
 		
-		System.out.println("After Removing all elements from al1 : "+al1.removeAll(al));
-		System.out.println(al1);*/
+	
+		ArrayListClient alc = new ArrayListClient();
+		alc.add("A");
+		alc.add("B");
+		alc.add("C");
+		alc.add("D");
+		alc.add("E");
+		//remove range is possible only by creating a list with class name and extending the class to arraylist
+		alc.removeRange(2, 4);
+		System.out.println("Remove Range : "+alc);
 		
-		al.set(6, 34);
-		System.out.println("After Replacing at 6th index : "+al);
+		
+		System.out.println("al elements : "+al);
+		System.out.println("al1 elements : "+al1);
+		al1.retainAll(al);
+		System.out.println("After Retaining All  : "+al1);
+
+		
+		al.set(8, 122);
+		System.out.println("After Replacing at 6th index in al: "+al);
+		
 		
 		System.out.println("Size : "+al.size());
 		
+		
+		System.out.println("Sublist From index 2 to index 6 in al : "+al.subList(2, 6));//including 2 index but excluding 8 index
+		
+		
+		al.toArray();
+		System.out.println("ArrayList to Array : "+al);
+		
+		//toArray(T[] a)
+		
+		System.out.println(al1);	
+		al1.trimToSize();
+		System.out.println("After trim to size : "+al1.size());//it trims the unused data
 	}
 
 }
