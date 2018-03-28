51c54a7d6383df10ed14d8e6356cdd4efc3f4975
sys-51 <sys-51@sys-50-PC>
2018-03-28 10:38:24 +0530

    Collections

--- src/LinkedListMethods/LinkedListClient.java
+++ src/LinkedListMethods/LinkedListClient.java
@@ -117,48 +117,50 @@ public class LinkedListClient{
 		System.out.println("After PollLast : "+ll);//34 is removed
 		
 		
-		ll.remove(2);
-		System.out.println("After removing 2 index 25 in ll : "+ll);
-		
-		//for integer elements you have to mention new Integer as object
-		ll.remove(new Integer(28));
-		System.out.println("After removing 28 al : "+ll);
-		//for characters no need of object
-		ll.add("E");
-		ll.add(54);
-		ll.add(120);
-		ll.add("E");
-		System.out.println("After adding E in al: "+ll);
-		ll.remove("E");
-		System.out.println("After removing E in al : "+ll);
-		
-		ll3.addAll(ll);
-		System.out.println("After Adding ll Elements to ll3"+ll3);
-		ll3.removeAll(ll);
-		System.out.println("After Removing Elements of ll in ll3"+ll3);
+		System.out.println("Pop: "+ll.pop());//removes first element from list
+		System.out.println("After Pop : "+ll);
 		
 		
-		System.out.println("ll elements : "+ll);
-		System.out.println("ll1 elements : "+ll1);
-		ll1.retainAll(ll);
-		System.out.println("After Retaining All  : "+ll1);
-
+		ll.push(24);// adds an element in first of list
+		System.out.println("After Push : "+ll);
 		
-		ll.set(8, 122);
-		System.out.println("After Replacing at 6th index in ll: "+ll);
+		
+		ll.remove(1);
+		System.out.println("After removing 1 index 25 in ll : "+ll);
+		
+		
+		ll.remove();//it removes head element
+		System.out.println("After remove : "+ll);
+		
+		//	ll.remove(o);
+		
+		System.out.println("After RemoveFirst : "+ll1.removeFirst());
+		
+		
+		System.out.println("Before RemoveFirstOccurance : "+ll1);
+		System.out.println("After RemoveFirstOccurance : "+ll1.removeFirstOccurrence(28));
+		System.out.println("After RemoveFirstOccurance : "+ll1);
+		
+		
+		System.out.println("After RemoveLast : "+ll1.removeLast());
+		
+		ll1.add(24);
+		System.out.println("Before RemoveLastOccurance : "+ll1);
+		System.out.println("After RemoveLastOccurance : "+ll1.removeLastOccurrence(24));
+		System.out.println("After RemoveLastOccurance : "+ll1);
+		
+				
+		ll1.set(6, 30);//replacing 34 with 30
+		System.out.println("After Replacing at 6th index in ll1: "+ll1);
 		
 		
 		System.out.println("Size : "+ll.size());
 		
 		
-		System.out.println("Sublist From index 2 to index 6 in ll : "+ll.subList(2, 6));//including 2 index but excluding 8 index
-		
-		
-		ll.toArray();
-		System.out.println("LinkedList to Array : "+ll);
+		ll1.toArray();
+		System.out.println("LinkedList to Array : "+ll1);
 		
 		//toArray(T[] a)
-		
 		
 	}
 
